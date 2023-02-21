ef7f8b09c42339ceb9e2b815b868f1049b0b675f
t-hein4 <th.hein04@gmail.com>
2023-02-21 13:49:00 -0800

    Update style

--- src/components/project.js
+++ src/components/project.js
@@ -15,29 +15,35 @@ export function Project({
   return (
     <div className="container mx-auto space-y-16 py-16">
       <div className="space-y-8 px-4 2xl:px-0">
-        <h1 className="text-center font-heading text-4xl font-medium md:text-left md:text-6xl md:font-semibold">
-          {title}
-        </h1>
+        {title && (
+          <h1 className="text-center font-heading text-4xl font-medium md:text-left md:text-6xl md:font-semibold">
+            {title}
+          </h1>
+        )}
         <div className="w-full max-w-prose">
-          <Author {...{ ...author }} />
+          {author && <Author {...{ ...author }} />}
         </div>
       </div>
       <div className="shadow-lg shadow-black/20">
-        <CoverImage cover_image={cover_image} title={title} />
+        {cover_image && <CoverImage cover_image={cover_image} title={title} />}
       </div>
       <div className="grid grid-flow-row justify-center gap-8 px-4 2xl:px-0">
-        <TechStack {...{ tech_stack }} />
-        <ProjectContent content={content} />
+        {tech_stack && <TechStack {...{ tech_stack }} />}
+        {content && <ProjectContent content={content} />}
       </div>
       <hr />
-      <div className="space-y-12">
-        <h2 className="px-4 text-center font-heading text-4xl font-medium md:text-left md:text-5xl 2xl:px-0">
-          More Projects
-        </h2>
-        {moreProjects.map((moreProject) => (
-          <ProjectCard {...{ ...moreProject }} key={moreProject._id} />
-        ))}
-      </div>
+      {moreProjects.length > 0 && (
+        <div className="space-y-12">
+          <h2 className="px-4 text-center font-heading text-4xl font-medium md:text-left md:text-5xl 2xl:px-0">
+            More Projects
+          </h2>
+          <div className="space-y-32">
+            {moreProjects.map((moreProject) => (
+              <ProjectCard {...{ ...moreProject }} key={moreProject._id} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -4,7 +4,7 @@ export function Projects({ projects }) {
   return (
     <>
       {projects.length > 0 && (
-        <div className="container mx-auto py-16">
+        <div className="container mx-auto space-y-32 py-16">
           {projects.map((project) => (
             <ProjectCard {...{ ...project }} key={project._id} />
           ))}

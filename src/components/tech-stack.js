5546e2fb9c1f75de797502cff5d4061c1bf1cb58
t-hein4 <th.hein04@gmail.com>
2023-02-21 04:29:24 -0800

    Fix tech stack component for mobile layout

--- src/components/tech-stack.js
+++ src/components/tech-stack.js
@@ -2,14 +2,14 @@ import { Logo } from "./logo";
 
 export function TechStack({ tech_stack }) {
   return (
-    <div className="flex items-center text-sm font-semibold">
-      <div className="pr-2">Built with:</div>
-      <div className="flex items-center divide-x divide-neutral-200">
+    <div className="flex flex-col text-sm font-semibold md:flex-row md:items-center">
+      <div className="pr-2 pb-2 md:pb-0">Built with:</div>
+      <div className="flex w-fit flex-col divide-y divide-neutral-200 md:w-full md:flex-row md:items-center md:divide-x md:divide-y-0">
         {tech_stack.map((tech) => (
           <a
             key={tech._id}
             href={tech.link}
-            className="flex items-center gap-2 px-3 hover:underline"
+            className="flex items-center gap-2 py-1 hover:underline md:px-3"
           >
             <Logo logo={tech.logo} name={tech.name} />
             <span className="italic">{tech.name}</span>

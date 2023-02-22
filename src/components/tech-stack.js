ba99af6979e8eed6ecedf424f46a4c988afb2b0b
t-hein4 <th.hein04@gmail.com>
2023-02-21 21:56:11 -0800

    Fix overlapping in large display

--- src/components/tech-stack.js
+++ src/components/tech-stack.js
@@ -2,20 +2,17 @@ import { Logo } from "./logo";
 
 export function TechStack({ tech_stack }) {
   return (
-    <div className="flex flex-col text-sm font-semibold lg:flex-row lg:items-center lg:text-base">
-      <div className="pr-2 pb-2 lg:w-32 lg:pb-0">Built with:</div>
-      <div className="flex w-fit flex-col divide-y divide-neutral-200 lg:w-full lg:flex-row lg:items-center lg:divide-x lg:divide-y-0">
-        {tech_stack.map((tech) => (
-          <a
-            key={tech._id}
-            href={tech.link}
-            className="flex items-center gap-2 py-1 hover:underline lg:px-3"
-          >
-            <Logo logo={tech.logo} name={tech.name} />
-            <span className="italic">{tech.name}</span>
-          </a>
-        ))}
-      </div>
+    <div className="flex flex-col divide-y divide-neutral-200 lg:flex-row lg:items-center lg:divide-x lg:divide-y-0">
+      {tech_stack.map((tech) => (
+        <a
+          key={tech._id}
+          href={tech.link}
+          className="flex items-center gap-2 py-1 hover:underline lg:flex-col lg:px-3"
+        >
+          <Logo logo={tech.logo} name={tech.name} />
+          <span className="text-sm italic">{tech.name}</span>
+        </a>
+      ))}
     </div>
   );
 }

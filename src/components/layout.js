af113e49c8653349ed768e38db7b013e6548e7d1
t-hein4 <th.hein04@gmail.com>
2023-02-22 13:17:00 -0800

    Remove max width on layout for '/studio' path

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,23 @@
+import clsx from "clsx";
+import { useRouter } from "next/router";
 import { Footer } from "./footer";
 import { Nav } from "./nav";
 import { PageTransition } from "./page-transition";
 
 export function Layout({ children }) {
+  const router = useRouter();
   return (
     <>
       <Nav />
       <PageTransition>
-        <div className="my-16 mx-auto max-w-6xl">{children}</div>
+        <div
+          className={clsx(
+            "my-16 mx-auto",
+            router.asPath !== "/studio" && "max-w-6xl"
+          )}
+        >
+          {children}
+        </div>
         <Footer />
       </PageTransition>
     </>

1732554f1305a3a087ec40560f981f467ed222bd
t-hein4 <th.hein04@gmail.com>
2023-02-21 22:46:28 -0800

    Update style

--- src/components/tech-stack.js
+++ src/components/tech-stack.js
@@ -2,15 +2,15 @@ import { Logo } from "./logo";
 
 export function TechStack({ tech_stack }) {
   return (
-    <div className="flex flex-col divide-y divide-neutral-200 lg:flex-row lg:items-center lg:divide-x lg:divide-y-0">
+    <div className="grid gap-y-2">
       {tech_stack.map((tech) => (
         <a
           key={tech._id}
           href={tech.link}
-          className="flex items-center gap-2 py-1 hover:underline lg:flex-col lg:px-3"
+          className="flex items-center gap-2 text-sky-800 hover:text-sky-500 hover:underline"
         >
           <Logo logo={tech.logo} name={tech.name} />
-          <span className="text-sm italic">{tech.name}</span>
+          <span className="text-sm">{tech.name}</span>
         </a>
       ))}
     </div>

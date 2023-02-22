f4331cca46f03189af6be2ac5967333c4aa9a2c0
t-hein4 <th.hein04@gmail.com>
2023-02-21 23:25:45 -0800

    Refactor componet to be more resilient

--- src/components/project-links.js
+++ src/components/project-links.js
@@ -1,18 +1,24 @@
 export function ProjectLinks({ source_code, live_demo }) {
   return (
-    <div className="flex flex-col justify-center gap-3">
-      <a
-        href={source_code}
-        className="cursor-pointer rounded-md bg-neutral-500 px-2 text-sm text-white shadow-md ring-1 ring-neutral-400/50 transition-all duration-150 ease-in-out hover:bg-white hover:text-neutral-800 hover:shadow-none"
-      >
-        Source code
-      </a>
-      <a
-        href={live_demo}
-        className="cursor-pointer rounded-md bg-neutral-500 px-2 text-sm text-white shadow-md ring-1 ring-neutral-400/50 transition-all duration-150 ease-in-out hover:bg-white hover:text-neutral-800 hover:shadow-none"
-      >
-        Live demo
-      </a>
-    </div>
+    (source_code || live_demo) && (
+      <div className="flex flex-col justify-center gap-3">
+        {source_code && (
+          <a
+            href={source_code}
+            className="cursor-pointer rounded-md bg-neutral-500 px-2 text-sm text-white shadow-md ring-1 ring-neutral-400/50 transition-all duration-150 ease-in-out hover:bg-white hover:text-neutral-800 hover:shadow-none"
+          >
+            Source code
+          </a>
+        )}
+        {live_demo && (
+          <a
+            href={live_demo}
+            className="cursor-pointer rounded-md bg-neutral-500 px-2 text-sm text-white shadow-md ring-1 ring-neutral-400/50 transition-all duration-150 ease-in-out hover:bg-white hover:text-neutral-800 hover:shadow-none"
+          >
+            Live demo
+          </a>
+        )}
+      </div>
+    )
   );
 }

d4bbf052b29c8d1e255ed7ca196ec0007fca0546
t-hein4 <th.hein04@gmail.com>
2023-02-22 03:17:54 -0800

    Update project links style

--- src/components/project-links.js
+++ src/components/project-links.js
@@ -5,7 +5,7 @@ export function ProjectLinks({ source_code, live_demo }) {
         {source_code && (
           <a
             href={source_code}
-            className="cursor-pointer rounded-md bg-neutral-500 px-2 text-sm text-white shadow-md ring-1 ring-neutral-400/50 transition-all duration-150 ease-in-out hover:bg-white hover:text-neutral-800 hover:shadow-none"
+            className="cursor-pointer rounded-md bg-white px-2 py-1 font-primary text-sm text-neutral-900 shadow-md ring-1 ring-neutral-400/50 transition-all duration-150 ease-in-out hover:bg-sky-700  hover:text-white hover:shadow-none"
           >
             Source code
           </a>
@@ -13,7 +13,7 @@ export function ProjectLinks({ source_code, live_demo }) {
         {live_demo && (
           <a
             href={live_demo}
-            className="cursor-pointer rounded-md bg-neutral-500 px-2 text-sm text-white shadow-md ring-1 ring-neutral-400/50 transition-all duration-150 ease-in-out hover:bg-white hover:text-neutral-800 hover:shadow-none"
+            className="cursor-pointer rounded-md bg-white px-2 py-1 font-primary text-sm text-neutral-900 shadow-md ring-1 ring-neutral-400/50 transition-all duration-150 ease-in-out hover:bg-sky-700  hover:text-white hover:shadow-none"
           >
             Live demo
           </a>

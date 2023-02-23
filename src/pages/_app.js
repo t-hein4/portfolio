07c21c4b3033f8482ffa2c051230324dbc851c4e
t-hein4 <th.hein04@gmail.com>
2023-02-23 11:34:45 -0800

    Add favicon and other meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,9 @@ import { Layout } from "@/components/layout";
 import { Nunito, Quicksand } from "@next/font/google";
 import { AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
+import Head from "next/head";
+import favicon from "../../public/favicon.svg";
+
 const quicksand = Quicksand({
   subsets: ["latin"],
   variable: "--font-quicksand",
@@ -15,16 +18,24 @@ const nunito = Nunito({
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   return (
-    <div className={`${quicksand.variable} ${nunito.variable}`}>
-      <AnimatePresence
-        mode="wait"
-        initial={false}
-        onExitComplete={() => window.scrollTo(0, 0)}
-      >
-        <Layout key={router.asPath}>
-          <Component {...pageProps} />
-        </Layout>
-      </AnimatePresence>
-    </div>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="icon" href={favicon.src} />
+      </Head>
+      <div className={`${quicksand.variable} ${nunito.variable}`}>
+        <AnimatePresence
+          mode="wait"
+          initial={false}
+          onExitComplete={() => window.scrollTo(0, 0)}
+        >
+          <Layout key={router.asPath}>
+            <Component {...pageProps} />
+          </Layout>
+        </AnimatePresence>
+      </div>
+    </>
   );
 }

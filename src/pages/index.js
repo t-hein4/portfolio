b51950c69452c6bfd52a715f1d3e099c11964a76
t-hein4 <th.hein04@gmail.com>
2023-02-23 11:41:37 -0800

    Add head tag with title, remove fetching  data

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,44 @@ import { Loading } from "@/components/loading";
 import { Projects } from "@/components/projects";
 import client from "@/lib/sanity-client";
 import { query } from "@/lib/sanity-queries";
+import clsx from "clsx";
 import { PreviewSuspense } from "next-sanity/preview";
+import Head from "next/head";
+import { useRouter } from "next/router";
 import { lazy } from "react";
+import { useEffect, useState } from "react";
+import { Banner } from "@/components/banner";
 const PreviewProjects = lazy(() => import("@/components/preview-projects"));
 export default function Home({ projects, preview }) {
+  const router = useRouter();
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const me = await client.fetch(query.aboutMe);
+      setData(me);
+    })();
+  }, []);
+
   return preview ? (
     <PreviewSuspense fallback={<Loading />}>
       <PreviewProjects query={query.allProjects} />
     </PreviewSuspense>
   ) : (
-    <Projects projects={projects} />
+    <>
+      <Head>
+        <title>Thein Hein</title>
+      </Head>
+      <Banner {...data} />
+      <div
+        className={clsx(
+          "my-16 mx-auto",
+          router.asPath !== "/studio" && "max-w-6xl"
+        )}
+      >
+        <Projects projects={projects} />
+      </div>
+    </>
   );
 }
 

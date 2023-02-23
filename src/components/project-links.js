b7e14463f3ee083228379c2a91739ecb00ba0c9b
t-hein4 <th.hein04@gmail.com>
2023-02-23 11:39:13 -0800

    Make separate component for link

--- src/components/project-links.js
+++ src/components/project-links.js
@@ -2,23 +2,32 @@ export function ProjectLinks({ source_code, live_demo }) {
   return (
     (source_code || live_demo) && (
       <div className="flex flex-col justify-center gap-3">
-        {source_code && (
-          <a
-            href={source_code}
-            className="cursor-pointer rounded-md bg-white px-2 py-1 font-primary text-sm text-neutral-900 shadow-md ring-1 ring-neutral-400/50 transition-all duration-150 ease-in-out hover:bg-sky-700  hover:text-white hover:shadow-none"
-          >
-            Source code
-          </a>
-        )}
-        {live_demo && (
-          <a
-            href={live_demo}
-            className="cursor-pointer rounded-md bg-white px-2 py-1 font-primary text-sm text-neutral-900 shadow-md ring-1 ring-neutral-400/50 transition-all duration-150 ease-in-out hover:bg-sky-700  hover:text-white hover:shadow-none"
-          >
-            Live demo
-          </a>
-        )}
+        {source_code && <ProjectLink href={source_code} name="Source code" />}
+        {live_demo && <ProjectLink href={live_demo} name="Live demo" />}
       </div>
     )
+  );
+}
+
+function ProjectLink({ href, name }) {
+  return (
+    <a
+      href={href}
+      className="group relative cursor-pointer rounded-md 
+      bg-gradient-to-bl from-sky-600 via-blue-800 to-sky-600 px-2 
+      py-1 font-primary text-sm font-semibold 
+      text-neutral-200 shadow-md ring-1 ring-neutral-400/50 hover:shadow-none"
+    >
+      <span
+        className="absolute inset-0 block h-full w-full cursor-pointer 
+        rounded-md bg-white bg-gradient-to-bl from-blue-800 
+      via-sky-600 to-blue-800 px-2 py-1 font-primary text-sm 
+      text-neutral-200 opacity-0 shadow-md transition-all duration-150
+        ease-linear group-hover:opacity-100 group-hover:shadow-none"
+      >
+        {name}
+      </span>
+      {name}
+    </a>
   );
 }

988c80056c15dc55223c9368688da0da5c840764
t-hein4 <th.hein04@gmail.com>
2023-02-23 11:40:06 -0800

    Change text color

--- src/components/tech-stack.js
+++ src/components/tech-stack.js
@@ -7,7 +7,7 @@ export function TechStack({ tech_stack }) {
         <a
           key={tech._id}
           href={tech.link}
-          className="flex items-center gap-2 text-sky-800 hover:text-sky-500 hover:underline"
+          className="flex items-center gap-2 text-blue-800 hover:text-blue-500 hover:underline"
         >
           <Logo logo={tech.logo} name={tech.name} />
           <span className="text-sm">{tech.name}</span>

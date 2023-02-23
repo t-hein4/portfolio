9b548b8500cebfac0b71a0fa0fde6a8875191d23
t-hein4 <th.hein04@gmail.com>
2023-02-23 11:34:56 -0800

    Remove cover-image

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -3,17 +3,10 @@ import { SanityBlock } from "@/components/sanity-block";
 import { CoverImage } from "@/components/cover-image";
 import { query } from "@/lib/sanity-queries";
 export default function AboutMe({ me }) {
-  const { about, cover_image, name } = me;
+  const { about } = me;
   return (
-    <div className="container mx-auto my-16 flex w-fit flex-col gap-16 xl:gap-24">
-      <div className="flex flex-col justify-center gap-x-16 gap-y-8">
-        <div className="shadow-lg shadow-black/30">
-          <CoverImage cover_image={cover_image} title={name} />
-        </div>
-        <div className="flex items-center justify-center px-4">
-          <SanityBlock value={about} />
-        </div>
-      </div>
+    <div className="my-32 flex items-center justify-center px-4">
+      <SanityBlock value={about} />
     </div>
   );
 }

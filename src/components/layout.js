d8fb0a6c1f044e5e51efc773a1ac4af19d8b04a6
t-hein4 <th.hein04@gmail.com>
2023-02-23 11:38:20 -0800

    Remove container

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,13 @@
-import clsx from "clsx";
-import { useRouter } from "next/router";
 import { Footer } from "./footer";
 import { Nav } from "./nav";
 import { PageTransition } from "./page-transition";
 
 export function Layout({ children }) {
-  const router = useRouter();
   return (
     <>
       <Nav />
       <PageTransition>
-        <div
-          className={clsx(
-            "my-16 mx-auto",
-            router.asPath !== "/studio" && "max-w-6xl"
-          )}
-        >
-          {children}
-        </div>
+        {children}
         <Footer />
       </PageTransition>
     </>
